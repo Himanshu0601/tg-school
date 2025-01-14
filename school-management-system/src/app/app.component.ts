bc291a37c091a80190d8bdaf73cd465fea0884a9
Himanshu Kumar <75099819+Himanshu0601@users.noreply.github.com>
2025-01-13 18:06:59 -0800

    init 2

--- school-management-system/src/app/app.component.ts
+++ school-management-system/src/app/app.component.ts
@@ -1,12 +1,25 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './admin/auth/login/login.component';
+import { NavigatorTopComponent } from './navigator/navigator-top/navigator-top.component';
+import { NavigatorLeftComponent } from './navigator/navigator-left/navigator-left.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    LoginComponent,
+    NavigatorTopComponent,
+    NavigatorLeftComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'school-management-system';
+
+  loggedIn : boolean = true;
+  isExpanded :boolean = false;
 }
---
+++ school-management-system/src/app/navigator/navigator-left/navigator-left.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigator-left',
+  imports: [],
+  templateUrl: './navigator-left.component.html',
+  styleUrl: './navigator-left.component.scss'
+})
+export class NavigatorLeftComponent {
+
+}
