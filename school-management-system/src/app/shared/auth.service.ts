e8c03e4b6633cb6734668e90b0520a38273fe44b
Himanshu Kumar <75099819+Himanshu0601@users.noreply.github.com>
2025-01-18 00:56:49 -0800

    Update auth.service.ts

--- school-management-system/src/app/shared/auth.service.ts
+++ school-management-system/src/app/shared/auth.service.ts
@@ -4,6 +4,33 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthService {
+   // Set a cookie
+    setCookie(name: string, value: string, path = '/', expires?: Date): void {
+    let cookieString = `${name}=${encodeURIComponent(value)}; path=${path}`;
+    if (expires) {
+      cookieString += `; expires=${expires.toUTCString()}`;
+    }
+    document.cookie = cookieString;
+  }
 
-  constructor() { }
+  // Get a cookie
+   getCookie(name: string): string | null {
+    const cookieArr = document.cookie.split(';');
+    for (const cookie of cookieArr) {
+      const [key, val] = cookie.trim().split('=');
+      if (key === name) {
+        return decodeURIComponent(val);
+      }
+    }
+    return null;
+  }
+
+  // Delete a cookie
+   deleteCookie(name: string, path = '/'): void {
+    document.cookie = `${name}=; path=${path}; expires=Thu, 01 Jan 1970 00:00:00 UTC`;
+  }
+
+  isAuthenticated(){
+    return !!this.getCookie('authToken')
+  }
 }
